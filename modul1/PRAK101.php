a5556455e2123fbe230511e4009ead3589e03c77
Fatur Rahman <101609961+fatrrr@users.noreply.github.com>
2022-03-18 15:45:25 +0800

    Add files via upload

---
+++ modul1/PRAK101.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+        <?php
+            echo "Hello World in PHP <br>";
+            echo "Nama Praktikan : Fatur Rahman <br>";
+            echo "NIM : 2010817310018";
+        ?>
+    </body>
+</html>
