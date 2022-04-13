2f8c85c369d19acca6b3eb4c7f6cc658267dfa9e
Fatur Rahman <101609961+fatrrr@users.noreply.github.com>
2022-04-13 21:34:30 +0800

    Update PRAK305.php
    change from header 3 to bold, enough ig

--- modul3/PRAK305.php
+++ modul3/PRAK305.php
@@ -15,8 +15,8 @@
             }
             $long = strlen($kalimat);
             $i = 0;
-            echo "<h3>Input : </h3>".$kalimat;
-            echo "<h3>Output : </h3>";
+            echo "<b>Input : </b><br>".$kalimat;
+            echo "<b>Output : </b><br>";
             while($i < $long){
                 $k = $long;
                 while($k > 0){
